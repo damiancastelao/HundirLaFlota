87b3f6cbab97f45cfd085e0ecdf22f6a61e5195f
Franco <fnarazaflores@danielcastelao.org>
2026-03-17 13:58:15 +0100

    hundir la flota

--- juego.py
+++ juego.py
@@ -1,13 +1,13 @@
 from tablero import Tablero
-
-
 class Juego:
     def __init__(self):
-        self.lanzar_ataque(4,2)
+        self.tablero = Tablero()
+        self.lanzar_ataque(1, 1)
+        self.lanzar_ataque(1, 2)
+        self.lanzar_ataque(1, 3)
+        self.lanzar_ataque(1,4)
+        self.lanzar_ataque(1, 5)
 
-
-    def inicializar_naves(self):
-        pass
 
     def mostrar_resultado(self, resultado):
         if resultado == 0:
@@ -15,13 +15,12 @@ class Juego:
         elif resultado == 1:
             print("Tocado")
         elif resultado == 2:
-            print("HundidO")
+            print("Hundido")
 
     def lanzar_ataque(self, x, y):
-        print(f"Atacando {x}, {y}")
-        obj_tablero = Tablero()
-        resultado = obj_tablero.comprobar_impacto(x, y)
+        print(f"Ataque a {x},{y}")
+        resultado = self.tablero.comprobar_impacto(x, y)
         self.mostrar_resultado(resultado)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Juego()
